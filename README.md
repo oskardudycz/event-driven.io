dcb3ee3b6a5062955aa60a68acb4e459f790347f
Oskar Dudycz <oskar.dudycz@gmail.com>
2020-12-02 15:55:04 +0100

    Added Google Tag Manager config

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Event-Driven.io
 
 Event-Driven.io - Resources about Event-Driven Architectures, Event Sourcing and pragmatic development
+
+
+## Included configuration
+
+1. Tracking with Google Analytics through Google Tag manager:
+- [Setting up the Google Analytics 4 Property with Google Tag Manager](https://www.youtube.com/watch?v=-J4feudVguc)
+- [Gatsby Google Tag Manager Config](https://www.gatsbyjs.com/plugins/gatsby-plugin-google-tagmanager/)
--- gatsby-config.js
+++ gatsby-config.js
@@ -209,13 +209,32 @@ module.exports = {
     //`gatsby-plugin-offline`,
     `gatsby-plugin-remove-serviceworker`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-tagmanager",
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-        cookieDomain: config.siteUrl,
-        defer: true,
-		    pageTransitionDelay: 50
-      }
+        id: process.env.GOOGLE_TAG_ID,
+
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+        // // Name of the event that is triggered
+        // // on every Gatsby route change.
+        // //
+        // // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
     },
     {
       resolve: `gatsby-plugin-feed`,
