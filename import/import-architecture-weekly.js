98729ad1ff10c34a42fa00821e54ce2c399bfe49
oskar.dudycz <oskar.dudycz@gmail.com>
2021-03-07 22:01:19 +0100

    Updated ArchitectureWeekly config

--- import/import-architecture-weekly.js
+++ import/import-architecture-weekly.js
@@ -7,10 +7,10 @@ const git = simpleGit();
         shell.rm("-rf", "./temp/ArchitectureWeekly");
         await git.clone("https://github.com/oskardudycz/ArchitectureWeekly.git", "./temp/ArchitectureWeekly");
         
-        shell.rm("-rf", "content/ArchitectureWeekly");
-        shell.mkdir("content/ArchitectureWeekly");
+        shell.rm("-rf", "content/architecture-weekly");
+        shell.mkdir("content/architecture-weekly");
         // shell.cp("-R", "./temp/content/posts/", "content/newsletter-pl");
-        shell.touch("content/ArchitectureWeekly/.gitkeep");
+        shell.touch("content/architecture-weekly/.gitkeep");
 
         console.log("SUCCESS! ArchitectureWeekly import succeeded.")
     } catch(error) {
