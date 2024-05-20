1cd5f30ca5d8a65a43cfc1dac07f3ce8e939e72f
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-20 10:25:39 +0200

    Updated Substack count

--- src/components/Post/Substack.js
+++ src/components/Post/Substack.js
@@ -9,7 +9,7 @@ const Substack = () => {
           one, <b>subscribe to Architecture Weekly.</b>
           <br />
           <br />
-          <b>✉️ Join over 5000 subscribers</b>, get the best resources to boost your skills, and
+          <b>✉️ Join over 5600 subscribers</b>, get the best resources to boost your skills, and
           stay updated with Software Architecture trends!
           <br />
           <br />
